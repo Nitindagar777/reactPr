7e7f6a83b94bc7019159dc2de12f051c072249c2
Nitin Dagar <nitindagar0800@gmail.com>
2026-08-06 21:57:38 +0530

    your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Footer from "./component/Footer";
 import UserList from "./component/UserList";
 import ProductList from "./component/ProductList";
 
+
+
 function App(){
   return (
     <>
@@ -14,11 +16,36 @@ function App(){
       {/* <h1>List of Users</h1> */}
       {/* <UserList /> */}
 
+      {/* 
       <h1>List of Products</h1>
-      <ProductList />
+      <ProductList /> */}
+
+      <Child name = "nitin" age = {22} status = "single" obj = {[{like : "cars", dislike : "nothing"}]} />
+      
     </>
   );
 
 }
+
+
+const Child  = ({name, age, status, obj}) => {
+    return(
+      <>
+        <div>
+          <h1>name : {name}</h1>
+          <h2>age : {age}</h2>
+          <h2>status: {status} </h2>
+          <h3>obj : {
+            obj.map((obj1, index) => (
+              <div key={index}>
+                <h1> like : {obj1.like} </h1>
+                <h1>dislike : {obj1.dislike}</h1>
+              </div>
+            ))
+            }</h3>
+        </div>
+      </>
+    );
+};
 
 export default App;
